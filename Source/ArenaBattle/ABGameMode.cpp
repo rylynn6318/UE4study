035f03d7941e94d380be3f87be4a944716be334b
donghyeon <rylynn6318@kumoh.ac.kr>
2021-02-23 15:26:53 +0900

    6장 캐릭터 컨트롤, 카메라 추가 및 변환 예제

--- Source/ArenaBattle/ABGameMode.cpp
+++ Source/ArenaBattle/ABGameMode.cpp
@@ -2,18 +2,14 @@
 
 
 #include "ABGameMode.h"
-#include "ABPawn.h"
+#include "ABCharacter.h"
 #include "ABPlayerController.h"
 
 AABGameMode::AABGameMode()
 {
-	DefaultPawnClass = AABPawn::StaticClass();
+	DefaultPawnClass = AABCharacter::StaticClass();
 	PlayerControllerClass = AABPlayerController::StaticClass();
-	//static ConstructorHelpers::FClassFinder<APawn> BP_PAWN_C(TEXT("/Game/ThirdPersonBP/Blueprints/ThirdPersonCharater.ThirdPersonCharacter_C"));
-	//if (BP_PAWN_C.Succeeded())
-	//{
-	//	DefaultPawnClass = BP_PAWN_C.Class;
-	//}
+
 }
 
 void AABGameMode::PostLogin(APlayerController* NewPlayer)
