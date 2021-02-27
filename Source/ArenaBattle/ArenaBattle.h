029671033934d1b057bb4aad703230ab5d3743b6
donghyeon <rylynn6318@kumoh.ac.kr>
2021-02-27 17:30:49 +0900

    assertion 매크로 ABCHECK 추가

--- Source/ArenaBattle/ArenaBattle.h
+++ Source/ArenaBattle/ArenaBattle.h
@@ -12,3 +12,4 @@ DECLARE_LOG_CATEGORY_EXTERN(ArenaBattle, Log, All);
 //정보에 형식 문자열로 추가 정보를 지정해 로그남김
 #define ABLOG(Verbosity, Format, ...) UE_LOG(ArenaBattle, Verbosity, TEXT("%s %s"), *ABLOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
 
+#define ABCHECK(Expr, ...) {if(!(Expr)){ABLOG(Error, TEXT("ASSERTION : %s"), TEXT("'"#Expr"'")); return __VA_ARGS__;}}
