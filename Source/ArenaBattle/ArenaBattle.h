aec09a35f97c009d3ccb0591383f352e0d306a32
donghyeon <rylynn6318@kumoh.ac.kr>
2021-03-27 00:47:46 +0900

    5장 까지 내용

---
+++ Source/ArenaBattle/ArenaBattle.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "EngineMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(ArenaBattle, Log, All);
+
+#define ABLOG_CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+// 코드가 들어있는 파일 이름과 함수, 라인 정보를 ArenaBattle 카테고리로 로그남김
+#define ABLOG_S(Verbosity) UE_LOG(ArenaBattle, Verbosity, TEXT("%s"), *ABLOG_CALLINFO)
+//정보에 형식 문자열로 추가 정보를 지정해 로그남김
+#define ABLOG(Verbosity, Format, ...) UE_LOG(ArenaBattle, Verbosity, TEXT("%s %s"), *ABLOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
+
+
